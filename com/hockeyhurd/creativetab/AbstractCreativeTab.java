5e4e5f56ad0cd0c3488f51f51bade5bbc4885e41
Nick Hurd <datcodplaya@gmail.com>
2014-09-14 17:21:44 -0400

    - AbstractCreativeTab(s) should now be fixed

--- com/hockeyhurd/creativetab/AbstractCreativeTab.java
+++ com/hockeyhurd/creativetab/AbstractCreativeTab.java
@@ -8,8 +8,11 @@ import net.minecraft.item.Item;
 
 public abstract class AbstractCreativeTab extends CreativeTabs {
 
+	private final String text;
+	
 	public AbstractCreativeTab(int par1, String par2) {
 		super(par1, par2);
+		this.text = par2;
 	}
 
 	@SideOnly(Side.CLIENT)
@@ -18,7 +21,7 @@ public abstract class AbstractCreativeTab extends CreativeTabs {
 	}
 	
 	public String getTranslatedTabLabel() {
-		return "HCoreLib";
+		return this.text;
 	}
 	
 	public boolean hasSearchBar() {
