322c76385dc39f0a40bdb1907bf925df93663698
Nick Hurd <datcodplaya@gmail.com>
2014-09-18 19:12:18 -0400

    - Changed mcmod.info - Added jdoc to TimeLapse class

--- com/hockeyhurd/api/math/TimeLapse.java
+++ com/hockeyhurd/api/math/TimeLapse.java
@@ -1,20 +1,27 @@
 package com.hockeyhurd.api.math;
 
+/**
+ * Time Lapse class for calculating the amount of time since class object initialization.
+ * 
+ * @author hockeyhurd
+ * @see <a href = "https://github.com/hockeyhurd/HCoreLib/blob/master/com/hockeyhurd/api/math/TimeLapse.java">https://github.com/hockeyhurd/HCoreLib/blob/master/com/hockeyhurd/api/math/TimeLapse.java</a>
+ */
 public class TimeLapse {
 
 	private long startTime;
 	private static final double ns = 1e6;
-	
+
 	/**
 	 * Inits start of time tracking.
 	 */
 	public TimeLapse() {
 		startTime = System.nanoTime();
 	}
-	
+
 	/**
 	 * Gives effective time from point of init.
-	 * @return effective time in ms. 
+	 * 
+	 * @return effective time in ms.
 	 */
 	public double getEffectiveTimeSince() {
 		long currentTime = System.nanoTime();
