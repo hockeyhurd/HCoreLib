3bd167c78656bf6258d0378a2afee845e8d7fe6b
Nick Hurd <datcodplaya@gmail.com>
2014-10-02 18:26:25 -0400

    - Added renderItem method

--- com/hockeyhurd/api/util/TessellatorHelper.java
+++ com/hockeyhurd/api/util/TessellatorHelper.java
@@ -2,17 +2,44 @@ package com.hockeyhurd.api.util;
 
 import net.minecraft.block.Block;
 import net.minecraft.client.renderer.Tessellator;
+import net.minecraft.item.ItemStack;
 import net.minecraft.util.IIcon;
+import net.minecraftforge.client.IItemRenderer.ItemRenderType;
 
+import org.lwjgl.opengl.GL11;
+
+/**
+ * Class used as helper for various tessllations with generic methods often used
+ * with lwjgl based graphical renderings. 
+ * 
+ * @author hockeyhurd
+ * @version Oct 2, 2014
+ */
 public class TessellatorHelper {
 
 	private IIcon icon;
+	private final Tessellator tess = Tessellator.instance;
+	private boolean testFlagColour = false;
 	
+	/**
+	 * Default constructor used to initialize an icon and new instance of this class.
+	 * @param icon = icon to use.
+	 */
 	public TessellatorHelper(IIcon icon) {
 		this.icon = icon;
 	}
 	
-	public void drawCuboid(Block block, int x, int y, int z, double scale) {
+	/**
+	 * Method used to draw generic cube/block with another smaller cube/block inside
+	 * itself through given parameters.
+	 * 
+	 * @param block = block to draw (inside). 
+	 * @param x = position x.
+	 * @param y = position y.
+	 * @param z = position z.
+	 * @param scale = scale of block inside (defaults to 0.9d if invalid).
+	 */
+	public void drawCuboid(Block block, float x, float y, float z, double scale) {
 		// change the passed integer coordinates into double precision floats, and set the height on the y axis
 		double xx = (double) x;
 		double yy = (double) y + 0.1d;
@@ -20,9 +47,6 @@ public class TessellatorHelper {
 
 		// this is the scale of the squares, in blocks
 		double scale_copy = scale > 0 && scale < 1 ?  scale : 0.9d;
-		
-		// Create Tessellator instance.
-		Tessellator tess = Tessellator.instance;
 
 		// set the uv coordinates
 		double minU = icon.getMinU();
@@ -82,6 +106,95 @@ public class TessellatorHelper {
 		tess.addVertexWithUV(xxMax, yy + 0.0d, zzMin, minU, maxV);
 		tess.addVertexWithUV(xxMax, yy + 0.0d, zzMax, maxU, maxV);
 		tess.addVertexWithUV(xxMin, yy + 0.0d, zzMax, maxU, minV);
+	}
+	
+	/**
+	 * Method used for rendering block as item.
+	 * Tweaked method based from open source work by @author TheGreyGhost which can be found below
+	 * @see <a href="https://github.com/TheGreyGhost/ItemRendering/blob/master/src/TestItemRendering/blocks/ItemBlockNumberedFaces1Renderer.java">github.com/../ItemBlockNumberedFaces1Renderer.java</a>.
+	 * 
+	 * @param type = type of rendering, inventory, entity item, etc.
+	 * @param item = copy of itemstack.
+	 * @param generalizeNormals = true set all sides as normals, else individual.
+	 */
+	public void renderItem(ItemRenderType type, ItemStack item, boolean generalizeNormals) {
+		tess.startDrawingQuads();
+
+		// adjust rendering space to match what caller expects
+		boolean mustTranslate = false;
+		switch (type) {
+			case EQUIPPED:
+			case EQUIPPED_FIRST_PERSON: {
+				break; // caller expects us to render over [0,0,0] to [1,1,1], no translation necessary
+			}
+			case ENTITY:
+			case INVENTORY: {
+				// translate our coordinates so that [0,0,0] to [1,1,1] translates to the [-0.5, -0.5, -0.5] to [0.5, 0.5, 0.5] expected by the caller.
+				GL11.glTranslatef(-0.5F, -0.5F, -0.5F);
+				mustTranslate = true;   // must undo the translation when we're finished rendering
+				break;
+			}
+			default:
+				break; // never here
+		}
+
+		// xpos face blue
+		IIcon icon = item.getItem().getIconFromDamage(5);
+		if (generalizeNormals) tess.setNormal(0.0F, 1.0F, 0.0F);
+		else if (!generalizeNormals) tess.setNormal(1.0F, 0.0F, 0.0F);
+		if (testFlagColour) tess.setColorOpaque(0, 0, 255);
+		tess.addVertexWithUV(1.0, 0.0, 0.0, (double) icon.getMaxU(), (double) icon.getMaxV());
+		tess.addVertexWithUV(1.0, 1.0, 0.0, (double) icon.getMaxU(), (double) icon.getMinV());
+		tess.addVertexWithUV(1.0, 1.0, 1.0, (double) icon.getMinU(), (double) icon.getMinV());
+		tess.addVertexWithUV(1.0, 0.0, 1.0, (double) icon.getMinU(), (double) icon.getMaxV());
+
+		// xneg face purple
+		icon = item.getItem().getIconFromDamage(4);
+		if (!generalizeNormals) tess.setNormal(-1.0F, 0.0F, 0.0F);
+		if (testFlagColour) tess.setColorOpaque(255, 0, 255);
+		tess.addVertexWithUV(0.0, 0.0, 1.0, (double) icon.getMaxU(), (double) icon.getMaxV());
+		tess.addVertexWithUV(0.0, 1.0, 1.0, (double) icon.getMaxU(), (double) icon.getMinV());
+		tess.addVertexWithUV(0.0, 1.0, 0.0, (double) icon.getMinU(), (double) icon.getMinV());
+		tess.addVertexWithUV(0.0, 0.0, 0.0, (double) icon.getMinU(), (double) icon.getMaxV());
+
+		// zneg face white
+		icon = item.getItem().getIconFromDamage(2);
+		if (!generalizeNormals) tess.setNormal(0.0F, 0.0F, -1.0F);
+		if (testFlagColour) tess.setColorOpaque(255, 255, 255);
+		tess.addVertexWithUV(0.0, 0.0, 0.0, (double) icon.getMaxU(), (double) icon.getMaxV());
+		tess.addVertexWithUV(0.0, 1.0, 0.0, (double) icon.getMaxU(), (double) icon.getMinV());
+		tess.addVertexWithUV(1.0, 1.0, 0.0, (double) icon.getMinU(), (double) icon.getMinV());
+		tess.addVertexWithUV(1.0, 0.0, 0.0, (double) icon.getMinU(), (double) icon.getMaxV());
+
+		// zpos face green
+		icon = item.getItem().getIconFromDamage(3);
+		if (!generalizeNormals) tess.setNormal(0.0F, 0.0F, -1.0F);
+		if (testFlagColour) tess.setColorOpaque(0, 255, 0);
+		tess.addVertexWithUV(1.0, 0.0, 1.0, (double) icon.getMaxU(), (double) icon.getMaxV());
+		tess.addVertexWithUV(1.0, 1.0, 1.0, (double) icon.getMaxU(), (double) icon.getMinV());
+		tess.addVertexWithUV(0.0, 1.0, 1.0, (double) icon.getMinU(), (double) icon.getMinV());
+		tess.addVertexWithUV(0.0, 0.0, 1.0, (double) icon.getMinU(), (double) icon.getMaxV());
+
+		// ypos face red
+		icon = item.getItem().getIconFromDamage(1);
+		if (!generalizeNormals) tess.setNormal(0.0F, 1.0F, 0.0F);
+		if (testFlagColour) tess.setColorOpaque(255, 0, 0);
+		tess.addVertexWithUV(1.0, 1.0, 1.0, (double) icon.getMaxU(), (double) icon.getMaxV());
+		tess.addVertexWithUV(1.0, 1.0, 0.0, (double) icon.getMaxU(), (double) icon.getMinV());
+		tess.addVertexWithUV(0.0, 1.0, 0.0, (double) icon.getMinU(), (double) icon.getMinV());
+		tess.addVertexWithUV(0.0, 1.0, 1.0, (double) icon.getMinU(), (double) icon.getMaxV());
+
+		// yneg face yellow
+		icon = item.getItem().getIconFromDamage(0);
+		if (!generalizeNormals) tess.setNormal(0.0F, -1.0F, 0.0F);
+		if (testFlagColour) tess.setColorOpaque(255, 255, 0);
+		tess.addVertexWithUV(0.0, 0.0, 1.0, (double) icon.getMaxU(), (double) icon.getMaxV());
+		tess.addVertexWithUV(0.0, 0.0, 0.0, (double) icon.getMaxU(), (double) icon.getMinV());
+		tess.addVertexWithUV(1.0, 0.0, 0.0, (double) icon.getMinU(), (double) icon.getMinV());
+		tess.addVertexWithUV(1.0, 0.0, 1.0, (double) icon.getMinU(), (double) icon.getMaxV());
+		tess.draw();
+		
+		if (mustTranslate) GL11.glTranslatef(0.5F, 0.5F, 0.5F);
 	}
 
 }
