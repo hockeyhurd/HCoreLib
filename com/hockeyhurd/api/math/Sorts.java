11dfbb8d853077d721909f03f450c8ab49867e16
hockeyhurd <nhurd96@gmail.com>
2016-01-27 18:02:47 -0500

    - Fixes to some sort methods and added documentation.

--- com/hockeyhurd/api/math/Sorts.java
+++ com/hockeyhurd/api/math/Sorts.java
@@ -8,6 +8,12 @@ package com.hockeyhurd.api.math;
  */
 public final class Sorts {
 
+    /**
+     * SortType enumeration.
+     *
+     * @author hockeyhurd
+     * @version 1/27/16
+     */
     public enum SortType {
         ASCENDING, DESCENDING;
     }
@@ -15,6 +21,13 @@ public final class Sorts {
     private Sorts() {
     }
 
+    /**
+     * Selction sorts int array.
+     *
+     * @param arr Int array.
+     * @param sortType SortType to use.
+     * @return Result; true for success, false for error.
+     */
     public static boolean selectionSorti(int[] arr, SortType sortType) {
         if (arr == null || arr.length == 0) return false;
 
@@ -37,6 +50,13 @@ public final class Sorts {
         return true;
     }
 
+    /**
+     * Selction sorts long array.
+     *
+     * @param arr Long array.
+     * @param sortType SortType to use.
+     * @return Result; true for success, false for error.
+     */
     public static boolean selectionSortl(long[] arr, SortType sortType) {
         if (arr == null || arr.length == 0) return false;
 
@@ -59,6 +79,13 @@ public final class Sorts {
         return true;
     }
 
+    /**
+     * Selction sorts float array.
+     *
+     * @param arr Float array.
+     * @param sortType SortType to use.
+     * @return Result; true for success, false for error.
+     */
     public static boolean selectionSortf(float[] arr, SortType sortType) {
         if (arr == null || arr.length == 0) return false;
 
@@ -81,6 +108,13 @@ public final class Sorts {
         return true;
     }
 
+    /**
+     * Selction sorts double array.
+     *
+     * @param arr Double array.
+     * @param sortType SortType to use.
+     * @return Result; true for success, false for error.
+     */
     public static boolean selectionSortd(double[] arr, SortType sortType) {
         if (arr == null || arr.length == 0) return false;
 
@@ -103,6 +137,13 @@ public final class Sorts {
         return true;
     }
 
+    /**
+     * Selction sorts a comparable object array.
+     *
+     * @param comparables A comparable array.
+     * @param sortType SortType to use.
+     * @return Result; true for success, false for error.
+     */
     @SuppressWarnings("unchecked")
     public static boolean selectionSort(Comparable[] comparables, SortType sortType) {
         if (comparables == null || comparables.length == 0) return false;
@@ -128,6 +169,14 @@ public final class Sorts {
         return true;
     }
 
+    /**
+     * Quick sorts an int array.
+     * @param arr Int array.
+     * @param lower Lower index bound.
+     * @param higher Upper index bound.
+     * @param sortType SortType to use.
+     * @return Result; true if success, false if failure.
+     */
     public static boolean quickSorti(int[] arr, final int lower, final int higher, SortType sortType) {
         if (arr == null || arr.length == 0 || lower > higher) return false;
 
@@ -144,6 +193,9 @@ public final class Sorts {
                 final int value = arr[i];
                 arr[i] = arr[j];
                 arr[j] = value;
+
+                i++;
+                j--;
             }
         }
 
@@ -154,6 +206,14 @@ public final class Sorts {
         return true;
     }
 
+    /**
+     * Quick sorts an long array.
+     * @param arr Long array.
+     * @param lower Lower index bound.
+     * @param higher Upper index bound.
+     * @param sortType SortType to use.
+     * @return Result; true if success, false if failure.
+     */
     public static boolean quickSortl(long[] arr, final int lower, final int higher, SortType sortType) {
         if (arr == null || arr.length == 0 || lower > higher) return false;
 
@@ -170,6 +230,9 @@ public final class Sorts {
                 final long value = arr[i];
                 arr[i] = arr[j];
                 arr[j] = value;
+
+                i++;
+                j--;
             }
         }
 
@@ -180,6 +243,14 @@ public final class Sorts {
         return true;
     }
 
+    /**
+     * Quick sorts an float array.
+     * @param arr Float array.
+     * @param lower Lower index bound.
+     * @param higher Upper index bound.
+     * @param sortType SortType to use.
+     * @return Result; true if success, false if failure.
+     */
     public static boolean quickSortf(float[] arr, final int lower, final int higher, SortType sortType) {
         if (arr == null || arr.length == 0 || lower > higher) return false;
 
@@ -196,6 +267,9 @@ public final class Sorts {
                 final float value = arr[i];
                 arr[i] = arr[j];
                 arr[j] = value;
+
+                i++;
+                j--;
             }
         }
 
@@ -206,6 +280,14 @@ public final class Sorts {
         return true;
     }
 
+    /**
+     * Quick sorts an double array.
+     * @param arr Double array.
+     * @param lower Lower index bound.
+     * @param higher Upper index bound.
+     * @param sortType SortType to use.
+     * @return Result; true if success, false if failure.
+     */
     public static boolean quickSortd(double[] arr, final int lower, final int higher, SortType sortType) {
         if (arr == null || arr.length == 0 || lower > higher) return false;
 
@@ -222,6 +304,9 @@ public final class Sorts {
                 final double value = arr[i];
                 arr[i] = arr[j];
                 arr[j] = value;
+
+                i++;
+                j--;
             }
         }
 
@@ -232,6 +317,15 @@ public final class Sorts {
         return true;
     }
 
+    /**
+     * Quick sorts a comparable object array.
+     *
+     * @param comparables Comparable object array.
+     * @param lower Lower index bound.
+     * @param higher Upper index bound.
+     * @param sortType SortType to use.
+     * @return Result; true if success, false if failure.
+     */
     @SuppressWarnings("unchecked")
     public static boolean quickSort(Comparable[] comparables, final int lower, final int higher, SortType sortType) {
         if (comparables == null || comparables.length == 0 || lower > higher) return false;
@@ -249,6 +343,9 @@ public final class Sorts {
                 final Comparable value = comparables[i];
                 comparables[i] = comparables[j];
                 comparables[j] = value;
+
+                i++;
+                j--;
             }
         }
 
